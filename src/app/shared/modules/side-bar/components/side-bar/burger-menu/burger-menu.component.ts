93dc9b6616ab7b9a9898e9f2576fc0598e4182d5
dimoybiyca <xar4ak7even5@gmail.com>
2023-03-06 10:57:12 +0200

    close sidebar on navigate

--- src/app/shared/modules/side-bar/components/side-bar/burger-menu/burger-menu.component.ts
+++ src/app/shared/modules/side-bar/components/side-bar/burger-menu/burger-menu.component.ts
@@ -1,15 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
 
 @Component({
   selector: 'app-burger-menu',
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss'],
 })
-export class BurgerMenuComponent {
+export class BurgerMenuComponent implements OnInit {
   @Output('isActive') isActive = new EventEmitter<boolean>();
   isBurgerOpen: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.initListeners();
+  }
+
+  initListeners() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.changeState();
+      }
+    });
+  }
+
   onBurgerClick() {
+    this.changeState();
+  }
+
+  changeState(): void {
     this.isBurgerOpen = !this.isBurgerOpen;
     this.isActive.emit(this.isBurgerOpen);
   }
